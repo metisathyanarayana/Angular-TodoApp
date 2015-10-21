3a2392b63a63e0f95c278356bffb80e9b0d4d295
unknown <mnaray@BLRMNARAY12383.sapient.com>
2015-10-21 14:37:06 +0530

    initial commit

---
+++ js/todo.js
@@ -0,0 +1,41 @@
+var todo = angular.module('todoApp', []);
+todo.controller('todoController', ['$scope', function($scope) {
+    $scope.todos = [{
+        'title': 'my first task',
+        'done': false
+    }];
+
+    $scope.addTodo = function() {
+        $scope.todos.push({
+            'title': $scope.todoItem
+        });
+        $scope.todoItem = '';
+    };
+    $scope.remove = function() {
+        $scope.todos = $scope.todos.filter(function(item) {
+            return !item.done
+        })
+    };
+    $scope.remaining = function() {
+        var count = 0;
+        angular.forEach($scope.todos, function(item) {
+            count += item.done ? 0 : 1;
+        });
+        return count;
+    };
+
+    $scope.selectAll = function() {
+
+
+        angular.forEach($scope.todos, function(item) {
+            item.done = $scope.selectedAll;
+        });
+    };
+    $scope.unselectAll = function() {
+
+        angular.forEach($scope.todos, function(item) {
+            item.done = false;
+        });
+    };
+
+}]);
